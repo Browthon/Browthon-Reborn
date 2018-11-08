293d897d9aba6a7be36bc371e29c768580f9c704
LavaPower <lavapower84@gmail.com>
2018-11-08 21:01:31 +0100

    Add urlInput in Browser

--- Core/browser.py
+++ Core/browser.py
@@ -4,12 +4,20 @@
 from PyQt5.QtWidgets import QWidget, QGridLayout, QMessageBox
 
 from Core.Widgets.browserWidget import BrowserWidget
+from Core.Widgets.urlInput import UrlInput
 
 
 class Browser(QWidget):
     def __init__(self):
         super(Browser, self).__init__()
         self.grid = QGridLayout()
+
         self.browserWidget = BrowserWidget(self)
+        self.urlInput = UrlInput(self)
+
+        self.grid.addWidget(self.browserWidget, 1, 0)
+        self.grid.addWidget(self.urlInput, 0, 0)
+
         self.setLayout(self.grid)
         self.show()
+    
