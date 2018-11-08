8ba45fdd5cacc0fe3287ecdd687e1f084071d078
LavaPower <lavapower84@gmail.com>
2018-11-08 21:01:10 +0100

    Change Browser in QWidget

--- Core/browser.py
+++ Core/browser.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3.7
 # coding: utf-8
 
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget, QGridLayout, QMessageBox
 
 from Core.Widgets.browserWidget import BrowserWidget
 
 
-class Browser(QMainWindow):
+class Browser(QWidget):
     def __init__(self):
         super(Browser, self).__init__()
         self.grid = QGridLayout()
