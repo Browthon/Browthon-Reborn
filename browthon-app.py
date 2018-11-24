cdfabdf2cdb2aa76ce7b696d0477bca81d628bea
LavaPower <lavapower84@gmail.com>
2018-11-24 21:05:32 +0100

    Add a check for if PyQt is installed

--- browthon-app.py
+++ browthon-app.py
@@ -2,6 +2,23 @@
 # coding: utf-8
 
 import sys
+import shlex
+import pip 
+
+try:
+    import PyQt5
+except:
+    try:
+        cmd = "install PyQt5"
+        pip.main(shlex.split(cmd))
+    except:
+        input("ERREUR : Impossible d'installer PyQt5\n")
+        sys.exit()
+try:
+    import PyQt5.QtWebEngine
+except:
+    input("ERREUR : Votre installation ne comporte pas QtWebEngine.\nVérifiez si vous avez la version 64bits de Python.\n")
+    sys.exit()
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
