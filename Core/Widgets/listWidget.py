9d79c5b3a5e4142d77b1238e4a26a281c8bf87d9
LavaPower <lavapower84@gmail.com>
2018-12-18 09:43:39 +0100

    Adapt listWidget to theme

--- Core/Widgets/listWidget.py
+++ Core/Widgets/listWidget.py
@@ -5,14 +5,21 @@ from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem
 
 
 class ListWidget(QTreeWidget):
-    def __init__(self, liste):
+    def __init__(self, liste, typedata="Default"):
         super(ListWidget, self).__init__()
         self.setColumnCount(4)
-        self.setHeaderLabels(["Nom", "URL", "Date", "ID"])
+        self.typedata = typedata
+        if self.typedata == "Themes":
+            self.setHeaderLabels(["Nom", "Auteur", "Description", "Dossier"])
+        else:
+            self.setHeaderLabels(["Nom", "URL", "Date", "ID"])
         self.liste = liste
         self.listeItem = []
         for i in self.liste:
-            item = QTreeWidgetItem(self, [i[1], i[2], i[3], str(i[0])])
+            if self.typedata == "Themes":
+                item = QTreeWidgetItem(self, [i["name"], i["author"], i["description"], i["folder"]])
+            else:
+                item = QTreeWidgetItem(self, [i[1], i[2], i[3], str(i[0])])
             self.listeItem.append(item)
         self.insertTopLevelItems(0, self.listeItem)
 
@@ -25,6 +32,9 @@ class ListWidget(QTreeWidget):
         self.deleteallitems()
         self.listeItem = []
         for i in self.liste:
-            item = QTreeWidgetItem(self, [i[1], i[2], i[3], str(i[0])])
+            if self.typedata == "Themes":
+                item = QTreeWidgetItem(self, [i["name"], i["author"], i["description"], i["folder"]])
+            else:
+                item = QTreeWidgetItem(self, [i[1], i[2], i[3], str(i[0])])
             self.listeItem.append(item)
         self.insertTopLevelItems(0, self.listeItem)
