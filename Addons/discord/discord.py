bfac9b661fd3d8e908ae65838b0530c8acde3ca7
LavaPower <lavapower84@gmail.com>
2018-12-23 00:34:51 +0100

    Update discord addon

--- Addons/discord/discord.py
+++ Addons/discord/discord.py
@@ -2,29 +2,29 @@
 # coding: utf-8
 
 import sys
+import os
 
 sys.path.append('..')
 
+try:
+    from pypresence import Presence, Activity
+    import time
+except ImportError:
+    print("PyPresence n'est pas installé sur votre machine.")
 
 class Discord:
     def __init__(self):
-        self.thread = 0
-
+        self.launch = True
+        
     def load(self, main):
-        try:
-            from pypresence import Presence
-            import time
-        except ImportError:
-            print("PyPresence n'est pas installé sur votre machine.")
-        else:
-            client_id = "526065920519700480"
-            self.RPC = Presence(client_id)
-            self.RPC.connect()
-            self.RPC.update(state="Navigateur internet en Python")
-
-    def unload(self, main):
-        self.thread.join(timeout=0)
-
+        self.launch = True
+        client_id = "526065920519700480"
+        RPC = Presence(client_id)
+        RPC.connect()
+        ac = Activity(RPC, large_image="logo", large_text="Browthon",
+                    small_image="logo", small_text=main.version,
+                     details="Par LavaPower", state="Navigateur web en Python")
+        ac.start = int(time.time())
 
 instance = Discord
 name = "discord"
