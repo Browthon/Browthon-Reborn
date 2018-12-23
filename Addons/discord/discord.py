d7845594e74d991f126da19cc28d8cc2b8b3e6c2
LavaPower <lavapower84@gmail.com>
2018-12-23 23:09:51 +0100

    Update discord addon

--- Addons/discord/discord.py
+++ Addons/discord/discord.py
@@ -9,22 +9,45 @@ sys.path.append('..')
 try:
     from pypresence import Presence, Activity
     import time
+    from threading import Thread
 except ImportError:
     print("PyPresence n'est pas installé sur votre machine.")
+
+
+class DiscordRPC(Thread):
+    def __init__(self, addon, main, rpc):
+        super(DiscordRPC, self).__init__()
+        self.addon = addon
+        self.main = main
+        self.rpc = rpc
+
+    def run(self):
+        self.rpc.connect()
+        ac = Activity(self.rpc, large_image="logo", large_text="Browthon",
+                      small_image="logo", small_text=self.main.version,
+                      details="Par LavaPower", state="Navigateur web en Python")
+        ac.start = int(time.time())
+        while self.addon.launch:
+            pass
+        self.rpc.close()
+
 
 class Discord:
     def __init__(self):
         self.launch = True
-        
+        self.thread = 0
+
     def load(self, main):
         self.launch = True
         client_id = "526065920519700480"
-        RPC = Presence(client_id)
-        RPC.connect()
-        ac = Activity(RPC, large_image="logo", large_text="Browthon",
-                    small_image="logo", small_text=main.version,
-                     details="Par LavaPower", state="Navigateur web en Python")
-        ac.start = int(time.time())
+        rpc = Presence(client_id)
+
+        self.thread = DiscordRPC(self, main, rpc)
+        self.thread.start()
+
+    def unload(self, main):
+        self.launch = False
+        self.thread.join()
 
 instance = Discord
 name = "discord"
