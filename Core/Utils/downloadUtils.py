bffa337f461998df91594954a473b8f9be0e283c
LavaPower <lavapower84@gmail.com>
2019-01-12 22:57:39 +0100

    Add a function to download a file witout qt system

--- Core/Utils/downloadUtils.py
+++ Core/Utils/downloadUtils.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 
 from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+
+from urllib.request import urlopen
 
 
 class DownloadSignal(QObject):
@@ -10,3 +13,18 @@ class DownloadSignal(QObject):
     def __init__(self, parent):
         super(DownloadSignal, self).__init__()
         self.parent = parent
+
+
+def downloadfile(main, url):
+    path = QFileDialog.getSaveFileName(main, "Sauver comme")
+    if not path[0]:
+        return
+    else:
+        try:
+            with open(path[0], 'wb') as file:
+                file.write(urlopen(url).read())
+        except Exception:
+            QMessageBox.warning(main, "Téléchargement arrêté", "Votre fichier (" + path[0] + ") a eu une erreur...")
+        else:
+            QMessageBox.information(main, "Téléchargement complété",
+                                    "Votre fichier ("+path[0]+") a bien été enregistré")
